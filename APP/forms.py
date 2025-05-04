babf7d315d77c345c9cb4c89eec96fc384fd7f79
Aldo Emilio Jurado Guadalupe <emilio.jurado@ciencias.unam.mx>
2025-05-04 02:53:56 -0600

    formulario guardando, faltan verificaciones

--- APP/forms.py
+++ APP/forms.py
@@ -3,13 +3,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Usuario
 
+class nameForm(forms.Form):
+    '''
+    Como los 3 son filtro entonces les agregamos la propiedad de que no sean obligatorios para hacer submit
+    '''
+    numcta = forms.CharField(label='numcta', max_length=100, required=False)
+    nombre = forms.CharField(label='nombre', max_length=100, required=False)
+    apellido = forms.CharField(label='apellido', max_length=100, required=False)
+
 class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
-        # Excluimos el id_usuario para que no se muestre en el formulario
-        exclude = ['id_usuario']
-        # También puedes usar fields si prefieres enumerarlos explícitamente
-        # fields = ['nombre', 'apellido_materno', 'apellido_paterno', 'correo', 'password', 'img_url', 'is_admin']
+        fields = ['nombre', 'apellido_paterno', 'apellido_materno', 'correo', 'password', 'img_url']
         widgets = {
-            'password': forms.PasswordInput(),
+            'password': forms.PasswordInput()
         }
