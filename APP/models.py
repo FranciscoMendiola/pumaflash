c9a0144ad8ed73c098b9b99d054eb9e2faeebb7b
Aldo Emilio Jurado Guadalupe <emilio.jurado@ciencias.unam.mx>
2025-05-16 13:34:19 -0600

    modelos para votaciones

--- APP/models.py
+++ APP/models.py
@@ -52,3 +52,36 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.id_sender.id_user.name + " - " + self.id_receiver.id_user.name
+
+
+class Category(models.Model):
+    id_category = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, unique=False)
+    img_url = models.ImageField(
+        upload_to='img/categorias', default='img/categorias/default.png')
+
+    def __str__(self):
+        return self.name
+
+
+class Award(models.Model):
+    id_award = models.AutoField(primary_key=True)
+    id_winner = models.ForeignKey(
+        Profile, on_delete=models.CASCADE)
+    id_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    id_group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id_winner.id_user) + " - " + str(self.id_category) + " - " + str(self.id_group)
+
+
+class Vote(models.Model):
+    id_vote = models.AutoField(primary_key=True)
+    voted_user = models.ForeignKey(
+        User, related_name='votes_received', on_delete=models.CASCADE)
+    voting_user = models.ForeignKey(
+        User, related_name='votes_cast', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Vote from {self.voting_user.username} to {self.voted_user.username} in {self.category.name}"
